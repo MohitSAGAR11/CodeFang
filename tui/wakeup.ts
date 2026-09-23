48a0a9ef40d06fe35f73fbcfb4e85d56265af97c
MohitSAGAR11 <mohitsagar378@gmail.com>
2026-09-23 11:38:07 +0530

    prog: made selection of sub-cli mode

--- tui/wakeup.ts
+++ tui/wakeup.ts
@@ -1,6 +1,7 @@
 import { select, isCancel } from "@clack/prompts";
 import chalk from "chalk";
 import figlet from "figlet";
+import { runCliMode } from "../modes/cli";
 
 const BANNER_FONT = "ANSI Shadow";
 const SHADOW = chalk.hex("#5b4d9e");
@@ -46,8 +47,8 @@ export async function runWakeup() {
   }
 
   if (mode == "cli") {
-    console.log(chalk.dim("\n Starting CLI mode .........\n"));
+    await runCliMode()
   } else if (mode == "telegram") {
-    console.log(chalk.dim("\n Telegram mode started ......... \n"));
+
   }
 }
