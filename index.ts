1515f4c106ea4e56a7bcbfe147a8f9f16e7449ca
MohitSAGAR11 <mohitsagar378@gmail.com>
2026-09-23 11:14:52 +0530

    added normal cli wakeup

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("codefang-build").description("CodeFang cli").version("0.0.1");
+
+program
+  .command("wakeup")
+  .description("Show the banner and pick cli or telegram mode")
+  .action(async () => {
+    console.log("Wakeup calling.......");
+  });
+
+await program.parseAsync(process.argv);
